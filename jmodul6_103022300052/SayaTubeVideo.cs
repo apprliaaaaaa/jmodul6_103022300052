40ccf143650ba4e9fe973090338503654f39f61e
apprliaaaaaa <liania470@gmail.com>
2025-03-26 06:45:42 +0700

    inisialisasi kelas awal

---
+++ jmodul6_103022300052/SayaTubeVideo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jmodul6_103022300052
+{
+    class SayaTubeVideo
+    {
+    }
+}
